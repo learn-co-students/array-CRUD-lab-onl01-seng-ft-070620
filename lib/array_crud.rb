76f50c31701412bde34f2b53ac776057543dc3d6
Ariel Barclay <mrsbarclay101008@gmail.com>
2020-07-08 17:44:00 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  sons = ["Ryan", "Evangle", "Increase", "Champion"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(sons = ["Ryan", "Evangle", "Increase", "Champion"], daugther = "Loyalty")
+ sons.push(daugther)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(sons = ["Ryan", "Evangle", "Increase", "Champion"], daugther = "Loyalty" )
+  sons.unshift(daugther)
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(sons = ["Ryan", "Evangle", "Increase", "Champion"])
+  sons.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(sons = ["Ryan", "Evangle", "Increase", "Champion"])
+  sons.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(sons, index )
+  sons[index]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(sons = ["Ryan", "Evangle", "Increase", "Champion"])
+  sons[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(sons = ["Ryan", "Evangle", "Increase", "Champion"])
+  sons[-1]
 end
